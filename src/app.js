28a0f8f36765e7e75908e602da6ce5b57ccf43e1
Daffa Maulana <daffamaulana30@gmail.com>
2026-05-18 13:06:53 +0700

    update script

--- src/app.js
+++ src/app.js
@@ -22,6 +22,16 @@ app.get("/", (req, res) => {
   res.json({ message: "API Wedding Organizer Management" });
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on ${process.env.PORT}`);
-});
+// app.listen(process.env.PORT, () => {
+//   console.log(`Server running on ${process.env.PORT}`);
+// });
+
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 5000;
+
+  app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`);
+  });
+}
+
+module.exports = app;
